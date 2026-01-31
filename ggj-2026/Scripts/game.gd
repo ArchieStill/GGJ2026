2065bd8e3fb9da57f479f40c043aff2f7b715cb4
Archie <archie2.still@live.uwe.ac.uk>
2026-01-31 14:01:19 +0000

    simon says finished and fully integrated

--- ggj-2026/Scripts/game.gd
+++ ggj-2026/Scripts/game.gd
@@ -11,12 +11,19 @@ func _on_interact_point_typing() -> void:
 func _on_typing_game_finished() -> void:
 	$Player.can_move = true
 
-func _on_interact_point_simonsays() -> void:
-	pass # Replace with function body.
-
 func _on_interact_point_coffee() -> void:
 	$CoffeeGame.show()
 	$CoffeeGame.can_play = true
 	$Player.can_move = false
 func _on_coffee_game_finished() -> void:
+	$Player.can_move = true
+
+func _on_interact_point_simonsays() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$SimonSaysGame.show()
+	$SimonSaysGame.start_timer.start()
+	$SimonSaysGame.can_play = true
+	$Player.can_move = false
+func _on_simon_says_game_finished() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
