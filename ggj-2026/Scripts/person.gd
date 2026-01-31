8199deec98bd8b2403e8d14112b88f0731288dd2
Nathan Scott <Nathan4.Scott@live.uwe.ac.uk>
2026-01-31 14:54:53 +0000

    npcs can navigate between points

--- ggj-2026/Scripts/person.gd
+++ ggj-2026/Scripts/person.gd
@@ -2,15 +2,19 @@ extends CharacterBody3D
 
 var sp_nums = [0,1,2]
 var spots = []
-var patrol = 0
+var patrol = -1
 var patrol_max
+
+var waiting = true
+@onready var timer = $Timer
 
 var shocked = false
 
 @onready var nav_agent: NavigationAgent3D = $NavigationAgent3D
 @onready var catcher = $Catchball/CollisionShape3D
 
-const SPEED = 5.0
+const walk = 5.0
+const chase = 7.5
 
 func _ready() -> void: #setup
 	var spot_root = get_node("/root/Node3D/People/NavigationRegion3D/Points")
@@ -18,6 +22,7 @@ func _ready() -> void: #setup
 	for i in sp_nums:
 		spots.append(spot_root.get_child(i))
 	patrol_max = sp_nums.size()
+	_point_reached()
 	pass
 
 func _navigate(target : Vector3): #sets new target location for npc
@@ -28,22 +33,38 @@ func _wander(pos : Node3D):
 	_navigate(pos.get_position())
 	pass
 
+func _point_reached():
+	if waiting:
+		patrol += 1
+		if patrol >= patrol_max:
+			patrol = 0
+		_wander(spots[patrol])
+		waiting = false
+	pass
+
+func _reach_check(now, next):
+	if (snapped(abs(now.z), 0.01) == snapped(abs(next.z), 0.01)):
+		if (snapped(abs(now.x), 0.01) == snapped(abs(next.x), 0.01)):
+			waiting = true
+			if timer.is_stopped():
+				timer.start()
+		elif waiting:
+			waiting = false
+	elif waiting:
+		waiting = false
+
 func _physics_process(delta: float) -> void: #moves npc
-	#if !shocked:
-	_navigate(Vector3(10,0,10))
-	if not is_on_floor():
-		velocity += get_gravity() * delta
-	var now_pos = Vector3(self.get_global_position())
-	var next_pos = Vector3(nav_agent.get_next_path_position())
-	var direction = now_pos.direction_to(next_pos)
-	print(direction)
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-	move_and_slide()
+	if !shocked:
+		if not is_on_floor():
+			velocity += get_gravity() * delta
+		var now_pos = Vector3(self.get_global_position())
+		var next_pos = Vector3(nav_agent.get_next_path_position())
+		var direction = now_pos.direction_to(next_pos)
+		var speed = walk
+		velocity.x = direction.x * speed
+		velocity.z = direction.z * speed
+		_reach_check(now_pos, next_pos)
+		move_and_slide()
 
 func _on_area_entered(body: Node3D) -> void:
 	#get disguise status
@@ -51,7 +72,7 @@ func _on_area_entered(body: Node3D) -> void:
 	if !disguise:
 		_navigate(body.get_position())
 		catcher.set_deferred("disabled", false)
-		#shocked = true
+		shocked = true
 	pass
 
 func _catch(): #for when person collider hits player
