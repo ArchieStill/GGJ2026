d41c17e8d77fe3735aae4199335e1c28f4b71020
JaDempsey673 <ja.dempsey2020@gmail.com>
2026-02-01 14:00:11 +0000

    Added music and music volume controls

--- ggj-2026/Scripts/Minigames/papergame.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-signal finished
-
-var paperInside = 0;
-var can_play = false
--- ggj-2026/scripts/pause_menu.gd
+++ ggj-2026/scripts/pause_menu.gd
@@ -4,8 +4,14 @@ var menu_open = false
 var mouse_already_visible = false
 @onready var main_audio_loop = $"../AudioStreamPlayer"
 
+var master_index= AudioServer.get_bus_index("Master")
+var BGM_index= AudioServer.get_bus_index("BGM")
+var SFX_index= AudioServer.get_bus_index("SFX")
+
 func _ready() -> void:
-	main_audio_loop.volume_linear = $AudioSlider.max_value
+	AudioServer.set_bus_volume_db(master_index,linear_to_db($MasterAudioSlider.max_value)) 
+	AudioServer.set_bus_volume_db(BGM_index,linear_to_db($BGMAudioSlider.max_value)) 
+	AudioServer.set_bus_volume_db(SFX_index,linear_to_db($SFXAudioSlider.max_value)) 
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_menu"):
@@ -27,4 +33,10 @@ func _on_quit_game_button_button_down() -> void:
 
 
 func _on_audio_slider_value_changed(value: float) -> void:
-	main_audio_loop.volume_linear = value
+	AudioServer.set_bus_volume_db(master_index,linear_to_db($MasterAudioSlider.value)) 
+	
+func _on_bgm_audio_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(BGM_index,linear_to_db($BGMAudioSlider.value)) 
+
+func _on_sfx_audio_slider_value_changed(value: float) -> void:
+	AudioServer.set_bus_volume_db(SFX_index,linear_to_db($SFXAudioSlider.value)) 
