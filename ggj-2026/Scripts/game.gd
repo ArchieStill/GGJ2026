f0841b025b861b6b47b3aece646c3cc6b8fc5f36
JaDempsey673 <ja.dempsey2020@gmail.com>
2026-02-01 11:37:04 +0000

    Basic Volume Control

--- ggj-2026/Scripts/game.gd
+++ ggj-2026/Scripts/game.gd
@@ -42,11 +42,11 @@ func _on_folder_game_finished() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
 
-func _on_interact_point_paper() -> void:
-	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	$PaperGame.show()
-	$PaperGame.can_play = true
-	$Player.can_move = false
+#func _on_interact_point_paper() -> void:
+	#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	#$PaperGame.show()
+	#$PaperGame.can_play = true
+	#$Player.can_move = false
 
 func _on_paper_game_finished() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
--- ggj-2026/scripts/pause_menu.gd
+++ ggj-2026/scripts/pause_menu.gd
@@ -1,7 +1,11 @@
 extends Control
 
 var menu_open = false
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+@onready var main_audio_loop = $"../AudioStreamPlayer"
+
+func _ready() -> void:
+	main_audio_loop.volume_linear = $AudioSlider.max_value
+
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("toggle_menu"):
 		if(menu_open):
@@ -15,3 +19,7 @@ func _process(delta: float) -> void:
 
 func _on_quit_game_button_button_down() -> void:
 	get_tree().quit()
+
+
+func _on_audio_slider_value_changed(value: float) -> void:
+	main_audio_loop.volume_linear = value
