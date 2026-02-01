5a775c64356b9486c4e062d1078c44820fdaed7c
Archie <archie2.still@live.uwe.ac.uk>
2026-02-01 15:43:21 +0000

    integrated folder game

--- ggj-2026/Scripts/game.gd
+++ ggj-2026/Scripts/game.gd
@@ -29,8 +29,27 @@ func _on_interact_point_simonsays() -> void:
 	$SimonSaysGame.start_timer.start()
 	$SimonSaysGame.can_play = true
 	$Player.can_move = false
-
 func _on_simon_says_game_finished() -> void:
+	Global.MinigamesCompleted += 1
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$Player.can_move = true
+
+func _on_interact_point_folder() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$FolderGame.show()
+	$FolderGame._toggle_can_play()
+	$Player.can_move = false
+func _on_folder_game_finished() -> void:
+	Global.MinigamesCompleted += 1
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$Player.can_move = true
+
+
+func _on_interact_point_paper() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$PaperGame.show()
+	$Player.can_move = false
+func _on_paper_game_finished() -> void:
 	Global.MinigamesCompleted += 1
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
