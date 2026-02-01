05f196fbddcb5fa0cf57751fb8e39b5513e6ae21
JaDempsey673 <ja.dempsey2020@gmail.com>
2026-02-01 11:12:15 +0000

    Fixed Folder game (I pray on the name of Barbatos)

--- ggj-2026/Scripts/Minigames/foldergame.gd
+++ ggj-2026/Scripts/Minigames/foldergame.gd
@@ -1,40 +1,45 @@
 extends Control
 
+signal finished
+
 var folder_scene = preload("res://Scenes/Minigames/FolderPreset.tscn")
 var foldersToSolve = 5
 var can_play = false
 
-func _ready() -> void:
+func _toggle_can_play():
+	can_play = !can_play
 	for i in range(5):
 		var new_num = randi() % Namelist.Text.size()
-		
 		$FolderController.get_child(i).get_child(4).text = Namelist.Text[new_num]
-		
 
 func _on_ah_body_entered(body: Node2D) -> void:
-	name = body.get_child(4).text
-	var letter_num = ord(name.left(1).to_upper()) - ord('A')
-	if (letter_num >= 0 && letter_num <= 7):
-		body.queue_free()
-		foldersToSolve -= 1
-		_check_finish()
+	if body.is_in_group("folder"):
+		name = body.get_child(4).text
+		var letter_num = ord(name.left(1).to_upper()) - ord('A')
+		if (letter_num >= 0 && letter_num <= 7):
+			body.queue_free()
+			foldersToSolve -= 1
+			_check_finish()
 
 func _on_ip_body_entered(body: Node2D) -> void:
-	name = body.get_child(4).text
-	var letter_num = ord(name.left(1).to_upper()) - ord('A')
-	if (letter_num >= 8 && letter_num <= 15):
-		body.queue_free()
-		foldersToSolve -= 1
-		_check_finish()
+	if body.is_in_group("folder"):
+		name = body.get_child(4).text
+		var letter_num = ord(name.left(1).to_upper()) - ord('A')
+		if (letter_num >= 8 && letter_num <= 15):
+			body.queue_free()
+			foldersToSolve -= 1
+			_check_finish()
 
 func _on_qz_body_entered(body: Node2D) -> void:
-	name = body.get_child(4).text
-	var letter_num = ord(name.left(1).to_upper()) - ord('A')
-	if (letter_num >= 16 && letter_num <= 25):
-		body.queue_free()
-		foldersToSolve -= 1
-		_check_finish()
+	if body.is_in_group("folder"):
+		name = body.get_child(4).text
+		var letter_num = ord(name.left(1).to_upper()) - ord('A')
+		if (letter_num >= 16 && letter_num <= 25):
+			body.queue_free()
+			foldersToSolve -= 1
+			_check_finish()
 		
 func _check_finish():
 	if foldersToSolve == 0:
-		print("won")
+		emit_signal("finished")
+		hide()
--- ggj-2026/Scripts/game.gd
+++ ggj-2026/Scripts/game.gd
@@ -3,11 +3,11 @@ extends Node3D
 func _on_mask_refill_refill() -> void:
 	$MaskHealth.value = 100
 
-
 func _on_interact_point_typing() -> void:
 	$TypingGame.show()
 	$TypingGame.can_play = true
 	$Player.can_move = false
+
 func _on_typing_game_finished() -> void:
 	$Player.can_move = true
 
@@ -16,6 +16,7 @@ func _on_interact_point_coffee() -> void:
 	$CoffeeGame.show()
 	$CoffeeGame.can_play = true
 	$Player.can_move = false
+	
 func _on_coffee_game_finished() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
@@ -26,22 +27,27 @@ func _on_interact_point_simonsays() -> void:
 	$SimonSaysGame.start_timer.start()
 	$SimonSaysGame.can_play = true
 	$Player.can_move = false
+
 func _on_simon_says_game_finished() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
 
-#func _on_interact_point_folder() -> void:
-	#Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
-	#$FolderGame.show()
-	#$FolderGame.can_play = true
-	#$Player.can_move = false
+func _on_interact_point_folder() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+	$FolderGame.show()
+	$FolderGame._toggle_can_play()
+	$Player.can_move = false
 
+func _on_folder_game_finished() -> void:
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+	$Player.can_move = true
 
 func _on_interact_point_paper() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
 	$PaperGame.show()
 	$PaperGame.can_play = true
 	$Player.can_move = false
+
 func _on_paper_game_finished() -> void:
 	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 	$Player.can_move = true
